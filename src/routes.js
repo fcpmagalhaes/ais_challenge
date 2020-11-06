04a604fc0d6e9ef136ccd9877415608f3d09774a
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-11-06 19:33:16 -0300

    Create and read users

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,33 @@
 const express = require('express');
-// const { celebrate, Segments, Joi } = require('celebrate');
+const { celebrate, Segments, Joi } = require('celebrate');
+
+const UserController = require('./controllers/UserController');
+const login = require('./middleware/Login');
+const adminVerify = require('./middleware/AdminVerify');
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => res.send('API running 🚀'));
+
+routes.get(
+  '/users',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  login, adminVerify, UserController.index,
+);
+
+routes.post(
+  '/users',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      login: Joi.string().required(),
+      password: Joi.string().required().length(6),
+    }),
+  }),
+  UserController.create,
+);
 
 module.exports = routes;
