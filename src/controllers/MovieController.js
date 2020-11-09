73859811f4a809c0aa92db8fed7be604d480a2b2
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-11-09 02:13:33 -0300

    Update movie controller to search at db before make api call

--- src/controllers/MovieController.js
+++ src/controllers/MovieController.js
@@ -1,33 +1,41 @@
-// const axios = require('axios');
+/* eslint-disable camelcase */
 const connection = require('../database/connection');
 const tmdbInstance = require('../services/axios/index');
 
 function apiGetMovie(id) {
-  return tmdbInstance.get(`/movie/${id}?api_key=${process.env.TMDB_KEY}`);
+  const params = {
+    api_key: process.env.TMDB_KEY,
+  };
+  return tmdbInstance.get(`/movie/${id}`, { params });
+}
+
+function recordMovieAtDatabase(movie) {
+  console.log(movie);
 }
 
 module.exports = {
-
   async index(req, res) {
     const { id } = req.params;
     try {
-      const movie = await apiGetMovie(id);
+      const movie = await connection('movies')
+        .where('id', id)
+        .select('*')
+        .first();
 
-      // const movie = await connection('movies')
-      //   .where('id', id)
-      //   .select('*')
-      //   .first();
-
-      // if (!movie) {
-      //   try {
-      //     console.log('vai procurar axios', process.env.TMDB_API);
-      //     console.log('vai procurar axios', process.env.NODE_ENV);
-      //   } catch (error) {
-      //     console.log('erro ao procurar axios', error);
-      //   }
-      // }
-
-      return res.status(200).send(movie.data);
+      if (!movie) {
+        try {
+          const { data } = await apiGetMovie(id);
+          recordMovieAtDatabase(data);
+          return res.status(200).send(data);
+        } catch (error) {
+          if (error.response) {
+            const { status_message } = error.response.data;
+            return res.status(error.response.status).json({ message: `${status_message}` });
+          }
+          return res.status(500).json({ error: 'Error on load movie' });
+        }
+      }
+      return res.status(200).send(movie);
     } catch (error) {
       return res.status(500).json({ error: 'Error on load movie' });
     }
