2c5c1f82cf26b81ea4b9dd547dfe93a6e04d7af3
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-11-09 07:37:06 -0300

    Add joins to get relationship values

--- src/controllers/MovieController.js
+++ src/controllers/MovieController.js
@@ -256,16 +256,30 @@ module.exports = {
         movie.belongs_to_collection = belongs_to_collection;
       }
 
-      // const genres = await connection('movie_genres')
-      //   .where('id_movie', movie.id)
-      //   .innerJoin('genres', 'genres.id_genre', 'id_movie')
-      //   .select('genres.*');
-      // console.log('genres', genres);
-      // production_companies =
-      // production_countries =
-      // spoken_languages =
+      const genres = await connection('movie_genres')
+        .where('id_movie', movie.id)
+        .innerJoin('genres', 'genres.id', 'movie_genres.id_genre')
+        .select('genres.id', 'genres.name');
 
-      // movie.genres = genres;
+      const production_companies = await connection('movie_production_companies')
+        .where('id_movie', movie.id)
+        .innerJoin('production_companies', 'production_companies.id', 'movie_production_companies.id_production_company')
+        .select('production_companies.*');
+
+      const production_countries = await connection('movie_production_countries')
+        .where('id_movie', movie.id)
+        .innerJoin('production_countries', 'production_countries.id', 'movie_production_countries.id_production_country')
+        .select('production_countries.*');
+
+      const spoken_languages = await connection('movie_spoken_languages')
+        .where('id_movie', movie.id)
+        .innerJoin('spoken_languages', 'spoken_languages.id', 'movie_spoken_languages.id_spoken_language')
+        .select('spoken_languages.*');
+
+      movie.genres = genres;
+      movie.production_companies = production_companies;
+      movie.production_countries = production_countries;
+      movie.spoken_languages = spoken_languages;
       return res.status(200).send(movie);
     } catch (error) {
       return res.status(500).json({ error: 'Error loading movie' });
